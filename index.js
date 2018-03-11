69d194a2205e4af2eec943311aa973b655c2b19d
Allar <allar.viinamae@hotmail.com>
2018-03-11 17:04:08 +0200

    Create OAuth authorize PoC

--- index.js
+++ index.js
@@ -1,43 +1,45 @@
-let express = require('express')
-let request = require('request')
-let querystring = require('querystring')
+let express = require('express');
+let Pipedrive = require('pipedrive');
 
-let app = express()
+let app = express();
 
 let redirect_uri =
   process.env.REDIRECT_URI ||
   'http://localhost:8888/callback'
 
 app.get('/login', function (req, res) {
-  res.redirect('https://oauth.pipedrive.com/oauth/authorize?' +
-    querystring.stringify({
-      client_id: process.env.PIPEDRIVE_CLIENT_ID,
-      redirect_uri,
-    }))
-})
+  console.log('login initialized');
+  Pipedrive.OAuthAuthenticate(
+    process.env.PIPEDRIVE_CLIENT_ID,
+    redirect_uri,
+    res
+  )
+});
 
 app.get('/callback', function (req, res) {
-  let code = req.query.code || null
-  let authOptions = {
-    url: 'https://oauth.pipedrive.com/oauth/token',
-    form: {
-      code: code,
-      redirect_uri,
-      grant_type: 'authorization_code'
-    },
-    headers: {
-      'Authorization': 'Basic ' + (new Buffer(
-        process.env.PIPEDRIVE_CLIENT_ID + ':' + process.env.PIPEDRIVE_CLIENT_SECRET
-      ).toString('base64'))
-    },
-    json: true
-  }
-  request.post(authOptions, function (error, response, body) {
-    const access_token = body.access_token
-    console.log('access token received', access_token);
-    res.redirect('http://localhost:8888' + '?access_token=' + access_token)
-  })
-})
+  Pipedrive.OAuthAuthenticateCallback(
+    req,
+    res,
+    redirect_uri,
+    process.env.PIPEDRIVE_CLIENT_ID,
+    process.env.PIPEDRIVE_CLIENT_SECRET
+  );
+});
+
+app.get('/app', function (req, res) {
+  let access_token = req.query.access_token || null;
+  console.log('access_token: ', access_token);
+
+  let pipedrive = new Pipedrive.Client(access_token, {strictMode: true});
+
+  pipedrive.Deals.getAll({}, function (err, deals) {
+    if (err) throw err;
+    for (var i = 0; i < deals.length; i++) {
+      console.log(deals[i].title + ' (worth ' + deals[i].value + ' ' + deals[i].currency + ')');
+    }
+  });
+});
+
 
 let port = process.env.PORT || 8888
 console.log(`Listening on port ${port}. Go /login to initiate authentication flow.`)
